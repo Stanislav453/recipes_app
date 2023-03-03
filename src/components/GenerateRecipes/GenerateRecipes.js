d32339839a97d282125a5c859deb8ae8c4c5276e
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-03 14:06:32 +0100

    creation function GenerateRecipes and add .generate-recipes-container, generate-recipes

---
+++ src/components/GenerateRecipes/GenerateRecipes.js
@@ -0,0 +1,9 @@
+import "./GenerateRecipes.scss"
+
+export const GenerateRecipes = () => {
+    return (
+        <div className="generate-recipes-container">
+            <button className="generate-recipes">Generate Recipes</button>
+        </div>
+    )
+}
