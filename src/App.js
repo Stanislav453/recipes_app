7103aa28405056d1a8daaa1bea44575f279aba44
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-03 09:54:43 +0100

    fetch data from API and added <Header />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.scss';
+import { Header } from './components/Header/Header';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export const App = () => {
+
+    //===HOOKS=========================================================================
+    const [ recipes, setRecipes ] = useState([])
+
+    //===API=============================================================================
+    const api_id  = "76c5daaa"
+    const api_key = "6074617b665371e229a7148394ec6bf2"
+    const request = `https://api.edamam.com/api/recipes/v2?type=public&q=hamburger%2Cchicken&app_id=${api_id}&app_key=${api_key}`
+  
+  
+    const [apiRequest, setApiRequest] = useState(request)
+  
+   //===FETCH_DATA_FROM_API=================================================================
+    const dataFromApi = () => {
+
+      fetch(apiRequest)
+      .then( (response) => response.json() )
+        .then((data) => setRecipes(data["hits"]))
+    }
+
+    useEffect(() => {
+      dataFromApi()
+    }, [])
+  
+
+console.log(  recipes )
+  
+  //===APP================================================================================
   return (
-    <section className="App">
-      <h1>This is recipes App</h1>
-    </section>
-  );
+    <div className='app'>
+      <Header />
+    </div>
+  )
 }
-
-export default App;
