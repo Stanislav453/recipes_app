e69cdd8930d6bfc6416f671b30f4817ef32c15b1
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-03 14:04:36 +0100

    deleted <main> and add .slim-text

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -2,25 +2,23 @@ import "./Recipes.scss"
 
 export const Recipes = ({recipes}) => {
     return (
-        <main>
             <div className="recipes-container">
                 <ul className="recipes-box-container">
                     {
                         recipes.map((item, key) => {
                             const { label, image, cuisineType, calories } = item.recipe
-                            return (
-                                <li key={key} className="recipes-box">
-                                    <img src={image} alt={label} />
-                                    <h3>{label}</h3>
-                                    <h4>cuisine type: {cuisineType}</h4>
-                                    <h4>calories: { Math.floor(calories) } </h4>
-                                    <button>show detail</button>
-                                </li>
-                                )
+                                return (
+                                    <li key={key} className="recipes-box">
+                                        <img src={image} alt={label} />
+                                        <h3>{label}</h3>
+                                        <h4>cuisine type: <span className="slim-text">{cuisineType}</span></h4>
+                                        <h4>calories: <span className="slim-text">{ Math.floor(calories) }kj</span> </h4>
+                                        <button>show detail</button>
+                                    </li>
+                                    )
                         } )
                     }
                 </ul>
             </div>
-        </main>
     )
 }
