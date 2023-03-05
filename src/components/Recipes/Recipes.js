4fc220896be73e89e89016c0e5ba4d97416faade
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-05 15:30:03 +0100

    import useState, Modal created hook recipesInfo and created function recipesInfoForModal and add this function to button inside .recipes-box

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,27 +1,37 @@
 import "./Recipes.scss"
-
+import React, { useState } from "react"
+import { Modal } from "../Modal/Modal"
 
 export const Recipes = ({ filteredData }) => {
 
+//=== HOOKS
+    const [recipesInfo, setRecipesInfo] = useState({ label: "", img: "", ingredientLines: "" })
 
+//=== SET_RECIPES_INFO_FOR_MODAL
+    const recipesInfoForModal = (label,img,ingredientLines) => {
+        setRecipesInfo({label,img,ingredientLines})
+    }
+    
+//=== RECIPES
     return (
             <div className="recipes-container">
                 <ul className="recipes-box-container">
                     {
                         filteredData.map((item, key) => {
-                            const { label, image, cuisineType, calories } = item.recipe
+                            const { label, image, cuisineType, calories, ingredientLines } = item.recipe
                                 return (
                                     <li key={key} className="recipes-box">
                                         <img src={image} alt={label} />
                                         <h3>{label}</h3>
                                         <h4>cuisine type: <span className="slim-text">{cuisineType}</span></h4>
                                         <h4>calories: <span className="slim-text">{ Math.floor(calories) }kj</span> </h4>
-                                        <button>show detail</button>
+                                        <button onClick={ () => recipesInfoForModal( label, image, ingredientLines ) } >show ingredients</button>
                                     </li>
                                     )
                         } )
                     }
-                </ul>
+            </ul>
+            { recipesInfo.label && <Modal recipesInfo={recipesInfo} setRecipesInfo={setRecipesInfo} />}
             </div>
     )
 }
