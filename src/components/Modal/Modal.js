d1a8fc97d39bd7838bc47633e38e3a5aea809755
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-05 15:23:37 +0100

    created component width Modal function and add classes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,25 @@
 import "./Modal.scss"
 
 
-export const Modal = () => {
-    return <h2>This is modal</h2>
+export const Modal = ({recipesInfo, setRecipesInfo}) => {
+    return (
+        <div className="modal-container">
+            <div className="modal">
+                <button onClick={ () => setRecipesInfo("") } className="modal-closed-button">X</button>
+                <div className="modal-left">
+                    <img className="modal-left-img" src={recipesInfo.img} />
+                    <h2 className="modal-left-title">{recipesInfo.label}</h2>
+                </div>
+                <div className="modal-right">
+                    <h2 className="modal-right-title">Indegrients</h2>
+                    <ul className="modal-right-list">
+                            {recipesInfo.ingredientLines.map((item, key) => {
+                                return <li className="modal-right-list-item" key={key} >{item}</li>
+                            } ) }              
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
