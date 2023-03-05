e35de1b459c3ef2f17ccd74155f4ee3bac3ba649
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-03-05 15:22:50 +0100

    create component width Footer function and add .footer, .footer-link

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.scss"
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <a className="footer-link" href="mailto:st.jendrisak@gmail.com">created by</a>
+        </footer>
+    )
+}
